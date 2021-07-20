7ab984216b140648a0b01622e226e10289e02371
Audry Ford <audryford792@gmail.com>
2021-07-20 13:40:43 -0500

    add link for more than 30 repos

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,14 +1,21 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
 	console.log(repo);
+
 	var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+
 	fetch(apiUrl).then(function (response) {
 		// request was successful
 		if (response.ok) {
 			response.json().then(function (data) {
-				// pass response data to dom function
 				displayIssues(data);
+
+				// check if api has paginated issues
+				if (response.headers.get("Link")) {
+					displayWarning(repo);
+				}
 			});
 		} else {
 			alert("There was a problem with your request!");
@@ -51,5 +58,17 @@ var displayIssues = function(issues) {
 	}
 };
 
+var displayWarning = function(repo) {
+	// add text to warning container
+	limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
-getRepoIssues("audryf/all-about-audry");
+	var linkEl = document.createElement("a");
+	linkEl.textContent = "See More issues on GitHub.com";
+	linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+	linkEl.setAttribute("target", "_blank");
+
+	// append to warning container
+	limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("angular/angular");
